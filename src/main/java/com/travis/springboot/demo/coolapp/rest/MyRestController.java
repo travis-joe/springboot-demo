0d270a0f5c5707180d9a7091a3be3bec9e36c5ff
qzy <weelgod@gmail.com>
2019-01-02 21:37:23 +0800

    521 spring boot employee crud

--- src/main/java/com/travis/springboot/demo/coolapp/rest/MyRestController.java
+++ src/main/java/com/travis/springboot/demo/coolapp/rest/MyRestController.java
@@ -1,5 +1,6 @@
 package com.travis.springboot.demo.coolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,22 @@ import java.time.LocalDateTime;
 
 @RestController
 public class MyRestController {
+
+    @Value("${coach.name}")
+    private String coach;
+
+    @Value("${team.name}")
+    private String team;
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello World Time is " + LocalDateTime.now();
     }
+
+    @GetMapping("/workout")
+    public String workout() {
+        return coach + " " +team;
+    }
+
+
 }
